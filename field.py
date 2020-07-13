2af48c844cc2fb511130451c4f86201b791b8091
K-Josai <tactful271828@gmail.com>
2020-07-13 23:34:10 +0900

    a

--- field.py
+++ field.py
@@ -9,8 +9,7 @@ class Field:
         #自由落下
         g = 9.8
         a = np.array([0, 0, -g])
-        x = self.ensemble.positions
         v = self.ensemble.velocities
         t = self.dt
         self.ensemble.positions += v*t + 1/2*a*(t**2)
-        self.ensemble.velocities += v + a*t
+        self.ensemble.velocities += a*t
