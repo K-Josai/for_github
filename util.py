71c89d64a82c3f0512715ce32146cd2c550f485d
elerac <maenosuke079@gmail.com>
2020-07-14 21:49:25 +0900

    Create util.py

---
+++ util.py
@@ -0,0 +1,7 @@
+
+def show_progress(i, max_iter, prog_size=50):
+    i+=1
+    progressd_size = int(i/max_iter*prog_size)
+    empty_size = int(prog_size - progressd_size)
+    pro_bar = ('=' * progressd_size) + (' ' * empty_size)
+    print('\r[{0}] {1}/{2}'.format(pro_bar, i, max_iter), end='')
