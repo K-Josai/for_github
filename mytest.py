9df1edfc9b97d13004e6cc0500ced80a48f03466
elerac <maenosuke079@gmail.com>
2020-07-14 14:22:42 +0900

    FreeFallField小クラスを使う

--- mytest.py
+++ mytest.py
@@ -1,34 +1,11 @@
 from particle import Particle
 from ensemble import Ensemble
-from field import Field
+from field import Field, FreeFallField
 import numpy as np
 
-def ensemble_test():
-    ATOM_NUM = 2
-
-    paricles = [Particle() for i in range(ATOM_NUM)]
-
-    ensemble = Ensemble(paricles)
-
-    print("位置の配列\n", ensemble.positions)
-    
-    ensemble.positions += 1 #全体に+1
-    print("+1\n", ensemble.positions)
-    
-    ensemble.positions *= 2 #全体に*2
-    print("*2\n", ensemble.positions)
-    
-    ensemble.positions *= np.array([1, 2, 3]) #x*1, y*2, z*3
-    print("x*1, y*2, z*3\n", ensemble.positions)
-    
-    ensemble.positions = np.ones((ensemble.N, 3))*100 #100にセット
-    print("=100\n", ensemble.positions)
-    
-    print("速度の配列\n", ensemble.velocities)
-
 def main():
-    ATOM_NUM = 1
-    CYCLE_NUM = 20
+    ATOM_NUM = 1 # Number of particles
+    CYCLE_NUM = 50 # Number of Cycle
 
     paricles = [Particle() for i in range(ATOM_NUM)]
 
@@ -38,7 +15,7 @@ def main():
     print("初期位置\n", ensemble.positions)
     print("初速度\n", ensemble.velocities)
 
-    myfield = Field(ensemble, dt=0.001)
+    myfield = FreeFallField(ensemble, dt=0.05)
     for i in range(CYCLE_NUM):
         myfield.update()
 
