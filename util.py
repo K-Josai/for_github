b528fa44abfdb22eb783c5cf02c1326af8d31a33
elerac <maenosuke079@gmail.com>
2020-07-14 22:00:05 +0900

    プログレスバーが最後に改行するように修正

--- util.py
+++ util.py
@@ -5,3 +5,4 @@ def show_progress(i, max_iter, prog_size=50):
     empty_size = int(prog_size - progressd_size)
     pro_bar = ('=' * progressd_size) + (' ' * empty_size)
     print('\r[{0}] {1}/{2}'.format(pro_bar, i, max_iter), end='')
+    if i>=max_iter: print()
