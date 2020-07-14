6b37e9ead932123d0745df67a3de76d2e6ed2832
elerac <maenosuke079@gmail.com>
2020-07-15 00:35:29 +0900

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -6,16 +6,16 @@ from util import show_progress
 import numpy as np
 
 def main():
-    ATOM_NUM = 500 # Number of partics
+    ATOM_NUM = 200 # Number of partics
     DENSITY = 0.8 # NUmber density
     CYCLE_NUM = 50 # Number of Cycle
-    DT = 0.0000001 # Time discritization
+    DT = 0.000001 # Time discritization
     R_CUT = 10 # Potencital Cutoff
     
     init_positions = np.random.rand(ATOM_NUM, 3) * 5
     init_positions[:,2] = 0
     #init_positions = np.array([[0, 1, 0], [0, 0, 0], [0, 2, 0]])
-    paricles = [Particle(init_positions[i]) for i in range(ATOM_NUM)]
+    paricles = [Particle(position=init_positions[i], mass=1) for i in range(ATOM_NUM)]
 
     ensemble = Ensemble(paricles)
 
