78881bb0a81ebe33fdd17670ba0b6789e98353c0
elerac <maenosuke079@gmail.com>
2020-07-15 10:07:49 +0900

    remove Kshisui

--- Kshisui/__init__.py
+++
@@ -1,5 +0,0 @@
-from .particle import Particle
-from .ensemble import Ensemble
-from .field import Field, LennardJonesField
-from .draw import drawMolecularAnimation2D
-from .util import show_progress
